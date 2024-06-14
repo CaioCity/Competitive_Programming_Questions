b1063bdb52a3b892e9c0c18bb6daf7d5c13852c0
CaioCity <160510210+CaioCity@users.noreply.github.com>
2024-06-14 00:52:47 -0300

    Update A.cpp

--- Codeforces/Contest1985/A.cpp
+++ Codeforces/Contest1985/A.cpp
@@ -1,2 +1,23 @@
 // https://codeforces.com/contest/1985/problem/A
 // Creating Words
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void build_crivo(int max, int* crivo);
+
+int main(){
+
+    short int N;
+    char a,b,c,d,e,f;
+
+    scanf("%hd",&N);
+
+    for(short int i=0; i<N; i++){
+        scanf(" %c %c %c %c %c %c", &d,&b,&c,&a,&e,&f);
+        printf("%c%c%c %c%c%c\n",a,b,c,d,e,f);
+    }
+    
+    return 0;
+}
+ 
