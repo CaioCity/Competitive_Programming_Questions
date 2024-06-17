1572d89e1a412d4b6e06cb443bc02b1ea04d6416
CaioCity <160510210+CaioCity@users.noreply.github.com>
2024-06-17 00:59:52 -0300

    Create A.cpp

---
+++ AtCoder/Contest358/A.cpp
@@ -0,0 +1,19 @@
+// https://atcoder.jp/contests/abc358/tasks/abc358_a
+// Welcome to AtCoder Land
+
+#include <bits/stdc++.h>
+
+int main() {
+
+    std::ios::sync_with_stdio(0),std::cin.tie(0),std::cout.tie(0);
+
+    std::string S,T;
+    std::cin>>S>>T;
+
+    if(S=="AtCoder" && T=="Land")
+        std::cout<<"Yes\n";
+    else std::cout<<"No\n";
+
+    return 0;
+}
+
