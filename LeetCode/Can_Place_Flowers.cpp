523e09e824e138fd5fa49108f3ce01747bfe8503
CaioCity <160510210+CaioCity@users.noreply.github.com>
2024-06-25 11:23:42 -0300

    Create Can_Place_Flowers.cpp

---
+++ LeetCode/Can_Place_Flowers.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/can-place-flowers/description/
+// Can Place Flowers
+
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int seq=1;
+        for(int i=0; i<flowerbed.size(); i++)
+            if(!flowerbed[i])
+                seq++;
+            else{
+                n-=((seq-1)/2);
+                if(n<=0) return 1;
+                seq=0;
+            }
+        n-=(seq/2);
+        if(n<=0)
+            return 1;
+        else return 0;
+
+
+    }
+};
