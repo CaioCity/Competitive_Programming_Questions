da96ef8d6bf0bd50d8f22d55c827323e45b2ffef
CaioCity <160510210+CaioCity@users.noreply.github.com>
2024-06-25 11:54:52 -0300

    Create Reverse_Prefix_of_Word.c

---
+++ LeetCode/Reverse_Prefix_of_Word.c
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/reverse-prefix-of-word/description/
+// Reverse Prefix of Word
+
+char* reversePrefix(char* word, char ch) {
+    short int tam=strlen(word),i;
+    
+    for(i=0; i<tam; i++)
+        if(word[i]==ch){
+            char aux;
+            for(short int j=0; j<i; j++, i--){
+                aux=word[j];
+                word[j]=word[i];
+                word[i]=aux;
+            }
+            break;
+        }
+            
+    return word;
+        
+}
