3541a47aca56f3dd015f79888ce78c1ed6fa7668
CaioCity <160510210+CaioCity@users.noreply.github.com>
2024-06-25 11:36:41 -0300

    Create Find_Common_Characters.cpp

---
+++ LeetCode/Find_Common_Characters.cpp
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/find-common-characters/
+// Find Common Characters
+
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        int tam=words.size();
+        map<string,int> resp, aux;
+        string S;
+
+        for(char x='a'; x<='z'; x++){
+            S=x;
+            aux[S]=0;
+        }
+        
+        for(auto x : words[0]){
+            S=x;
+            resp[S]++;
+        }
+            
+        for(int i=1; i<tam; i++){
+            for(auto x : words[i]){
+                S=x;
+                aux[S]++;
+            }
+                
+            for( auto& [k,v] : aux ){
+                resp[k]= resp[k]<v?resp[k]:v;
+                v=0;
+            }     
+        }
+
+        vector<string> answer;
+        for(auto [a,b] : resp)
+            while(b--)
+                answer.push_back(a);
+        
+        return answer;        
+    }
+};
