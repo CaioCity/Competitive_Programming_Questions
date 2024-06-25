6941b8939c3bae7ece818c887f76f13c0a9ac9d2
CaioCity <160510210+CaioCity@users.noreply.github.com>
2024-06-25 11:32:50 -0300

    Create Divide_Array_in_Sets_of_K_Consecutive_Numbers.cpp

---
+++ LeetCode/Divide_Array_in_Sets_of_K_Consecutive_Numbers.cpp
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/divide-array-in-sets-of-k-consecutive-numbers/description/
+// Divide Array in Sets of K Consecutive Numbers
+
+class Solution {
+public:
+    bool isPossibleDivide(vector<int>& nums, int K) {
+
+        if(nums.size()%K!=0)
+            return 0;
+        if(nums.size()==1)
+            return 1;
+
+        map<int,int> numbers;
+        for(int i :nums)
+            numbers[i]++;
+        
+        map<int,int>::iterator fim=numbers.end();
+        for(int i=1; i<K; i++)
+            fim--;
+        for(auto i=numbers.begin(); i!=fim; i++)
+            while(i->second--!=0)
+                for(int j=1; j<K; j++){
+                    if(numbers[i->first+j]>0)
+                        numbers[i->first+j]--;
+                    else
+                        return 0;
+                }
+
+        fim=numbers.end()--;
+        for(int i=0; i<K; i++, fim--)
+            if(fim->second!=0)
+                return false;            
+
+        return 1;
+    }
+};  
