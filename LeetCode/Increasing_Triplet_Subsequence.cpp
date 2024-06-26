7cedc54b1520a110b5ad78716112aed4ba55828e
CaioCity <160510210+CaioCity@users.noreply.github.com>
2024-06-26 20:14:50 -0300

    Create Increasing_Triplet_Subsequence.cpp

---
+++ LeetCode/Increasing_Triplet_Subsequence.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/increasing-triplet-subsequence/description
+// Increasing Triplet Subsequence
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int menor=nums[0], menor2=INT32_MAX, menorpos=INT32_MAX, tam=nums.size();
+        for(int i=1; i<tam; ++i){
+            if(nums[i]>menor2)
+                return 1;
+            if(nums[i]>menor && nums[i]<menor2)
+                menor2=nums[i];
+            if(nums[i]<menorpos && menor>nums[i])
+                menorpos=nums[i];
+            if(nums[i]>menorpos && nums[i]<menor2){
+                menor2=nums[i];
+                menor=nums[i];
+            }
+        }
+        return 0;
+    }
+};
