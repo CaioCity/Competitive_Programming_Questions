8e5ae185bbfbbc55f436788c479be0e406aeec8c
CaioCity <160510210+CaioCity@users.noreply.github.com>
2024-06-26 19:52:40 -0300

    Create Product_of_Array_Except_Self.cpp

---
+++ LeetCode/Product_of_Array_Except_Self.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/product-of-array-except-self/description
+// Product of Array Except Self
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int tam=nums.size();
+        vector<int> prefix(tam), suffix(tam), ans(tam);
+        prefix[0]=nums[0];
+        for(int i=1; i<tam; ++i)
+            prefix[i]=nums[i]*prefix[i-1];
+        suffix[tam-1]=nums[tam-1];
+        for(int i=tam-2; i>=0; --i)
+            suffix[i]=suffix[i+1]*nums[i];
+        
+        ans[0]=suffix[1];
+        ans[tam-1]=prefix[tam-2];
+        for(int i=1; i<(tam-1); ++i)
+            ans[i]=prefix[i-1]*suffix[i+1];
+        return ans;
+    }
+};
