e48354315514d19f724c82a4354a2b2e19747a15
CaioCity <160510210+CaioCity@users.noreply.github.com>
2024-06-26 13:54:43 -0300

    Create Minimum_Operations_to_Make_Binary_Array_Elements_Equal_to_One_I.cpp

---
+++ LeetCode/Minimum_Operations_to_Make_Binary_Array_Elements_Equal_to_One_I.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/minimum-operations-to-make-binary-array-elements-equal-to-one-i/
+// Minimum Operations to Make Binary Array Elements Equal to One I
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int loop=nums.size()-2,ans=0;
+    
+        for(int i=0; i<loop; ++i)
+            if(!nums[i]){
+                nums[i] = 1;
+                nums[i+1] = nums[i+1]? 0 : 1;
+                nums[i+2] = nums[i+2]? 0 : 1;
+                ++ans;
+            }
+
+        if(!nums[loop] || !nums[loop+1])
+            return -1;
+        return ans;
+    }
+};
