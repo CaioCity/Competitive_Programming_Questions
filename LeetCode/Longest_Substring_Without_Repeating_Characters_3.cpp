b531be4181e17249cff7d845283f98fd4e380cd8
CaioCity <160510210+CaioCity@users.noreply.github.com>
2024-06-27 19:30:17 -0300

    Update and rename Longest_Substring_Without_Repeating_Characters.cpp to Longest_Substring_Without_Repeating_Characters_3.cpp

--- LeetCode/Longest_Substring_Without_Repeating_Characters_3.cpp
+++ LeetCode/Longest_Substring_Without_Repeating_Characters_3.cpp
@@ -1,5 +1,5 @@
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
-// Longest Substring Without Repeating Characters
+// 3 - Longest Substring Without Repeating Characters
 
 class Solution {
 public:
