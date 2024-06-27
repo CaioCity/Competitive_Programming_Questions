18f4bced8712b8a26e514bce992d4032dc8d1983
CaioCity <160510210+CaioCity@users.noreply.github.com>
2024-06-26 22:56:18 -0300

    Create Max_Number_of_K-Sum_Pairs.cpp

---
+++ LeetCode/Max_Number_of_K-Sum_Pairs.cpp
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/max-number-of-k-sum-pairs/description
+// Max Number of K-Sum Pairs
+
+// Two Pointers
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        int ans=0,sum;
+        std::sort(nums.begin(),nums.end());
+        for(int i=0, j=nums.size()-1; i<j; ){
+            sum=nums[i]+nums[j];
+            if(sum>k)
+                --j;
+            else if(sum<k)
+                ++i;
+            else {
+                ++i; --j;
+                ++ans;
+            }
+        }
+        return ans;
+    }
+};
+
+// Hash Map
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        int ans=0;
+        unordered_map<int,int> sum;
+        for(auto i : nums){
+            if(sum[k-i]!=0){
+                ++ans;
+                --sum[k-i];
+            }
+            else sum[i]++;
+        }
+        return ans;
+    }
+};
