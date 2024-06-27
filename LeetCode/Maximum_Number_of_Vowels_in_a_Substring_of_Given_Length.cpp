7c2fca65f3908602a322421eeb8633cfba586713
CaioCity <160510210+CaioCity@users.noreply.github.com>
2024-06-26 23:49:19 -0300

    Create Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length.cpp

---
+++ LeetCode/Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/description
+// Maximum Number of Vowels in a Substring of Given Length
+
+class Solution {
+public:
+    bool vowel(char x){
+        if(x=='a'||x=='e'||x=='i'||x=='o'||x=='u')
+            return 1;
+        return 0;
+    }
+    int maxVowels(string s, int k) {
+        int loop=s.size()-k,max, atual=0;
+        for(int i=0; i<k; ++i)
+            if(vowel(s[i]))
+                ++atual;
+        max=atual;
+        for(int i=1; i<=loop; ++i){
+            if(vowel(s[i-1]))
+                --atual;
+            if(vowel(s[i+k-1]))
+                ++atual;
+            max=std::max(atual,max);
+        }
+        return max;
+    }
+};
