f3a1b956657828e64f93cb0a1af6deb22f223b20
CaioCity <160510210+CaioCity@users.noreply.github.com>
2024-06-26 22:36:58 -0300

    Create Container_With_Most_Water.cpp

---
+++ LeetCode/Container_With_Most_Water.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/container-with-most-water/description
+// Container With Most Water
+
+class Solution {
+public:
+    int area (int a, int b, vector<int>& h){
+        return (b-a)*std::min(h[a],h[b]);
+    }
+    int maxArea(vector<int>& height) {
+        int left=0,right=height.size()-1,max=0;
+        for(; left<right; ){
+            max=std::max(max,(right-left)*std::min(height[left],height[right]));
+            if(height[left]<height[right])
+                ++left;
+            else --right;
+        }
+        return max;
+    }
+};
