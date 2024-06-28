3fa4465ae115ddd42c3e85303111a0bfbd2e286e
CaioCity <160510210+CaioCity@users.noreply.github.com>
2024-06-28 11:49:45 -0300

    Create Find_Pivot_Index_724.cpp

---
+++ LeetCode/Find_Pivot_Index_724.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/find-pivot-index
+// 724 - Find pivot index
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int tam=nums.size(),aux=nums[0];
+        vector<int> suffix(tam);
+        suffix[tam-1]=nums[tam-1];
+
+        for(int i=tam-2; i>=0; --i)    
+            suffix[i]=suffix[i+1]+nums[i];
+        
+        if(suffix[0]-nums[0]==0)
+            return 0;
+
+        for(int i=1; i<(tam-1); ++i){
+            std::cout<<aux<<" x "<<suffix[i+1]<<'\n';
+            if(aux==suffix[i+1])
+                return i;
+            aux+=nums[i];
+        }
+
+        if(aux==0)
+            return tam-1;
+        
+        return -1;
+    }
+};
