a29efde059a378a50931d72faa8486b636f73bbc
CaioCity <160510210+CaioCity@users.noreply.github.com>
2024-07-01 15:13:38 -0300

    Create Copa_Neps203.cpp

---
+++ Neps/Copa_Neps203.cpp
@@ -0,0 +1,45 @@
+// https://neps.academy/br/exercise/203
+// Copa
+
+#include <bits/stdc++.h>
+
+int bs( int inicio, int fim, int N, int target, int V[] );
+
+int64_t coins (int dia, int N, int V[]);
+
+int main() {
+
+    std::ios::sync_with_stdio(0), std::cin.tie(0), std::cout.tie(0);
+
+    int K,L;
+    std::cin>>K>>L;
+
+    int pot=8;
+
+    std::map<int,std::string> ans;
+
+    ans[8]="final";
+    ans[4]="semifinal";
+    ans[2]="quartas";
+    ans[1]="oitavas";
+
+    while(pot!=1){
+
+        if(K>pot ^ L>pot){
+            std::cout<<ans[pot]<<'\n';
+        return 0;
+        }
+
+        if(K>pot){
+            K-=pot;
+            L-=pot;
+        }
+
+        pot>>=1;
+        // std::cout<<"pot = "<<pot<<'\n';
+    }
+
+    std::cout<<"oitavas\n";
+
+    return 0;
+}
