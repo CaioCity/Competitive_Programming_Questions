9a2f2a013797516cf02dc76bf145133b34479b41
CaioCity <160510210+CaioCity@users.noreply.github.com>
2024-08-28 00:22:48 -0300

    Update C.cpp

--- AtCoder/Contest368/C.cpp
+++ AtCoder/Contest368/C.cpp
@@ -1,6 +1,8 @@
 // https://atcoder.jp/contests/abc368/tasks/abc368_c
 // C - Triple Attack
 
+// Seguem 2 codes, o institivo e o otimizado em space O(1)
+
 #include <bits/stdc++.h>
 
 #define int int64_t
@@ -25,6 +27,37 @@ int32_t main(){
             if(ans%3==0)
                 V[i]-=3;
             else --V[i];          
+        }
+    }
+
+    std::cout<<ans<<'\n';
+
+    return 0;
+}
+
+// Space O(1)
+#include <bits/stdc++.h>
+
+#define int int64_t
+
+int32_t main(){
+
+    std::ios::sync_with_stdio(0), std::cin.tie(0);
+
+    int N,ans=0;
+    std::cin>>N;    
+
+    int giant;
+    
+    for(int i=0; i<N; ++i){
+        std::cin>>giant;
+        ans += 3*(giant/5);
+        giant%=5;
+        while(giant>0){
+            ++ans;
+            if(ans%3==0)
+                giant-=3;
+            else --giant;          
         }
     }
 
