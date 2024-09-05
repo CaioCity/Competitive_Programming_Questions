2f102aa6afe861acfbd3f8b3c6fb88e2d2a74500
CaioCity <160510210+CaioCity@users.noreply.github.com>
2024-09-04 22:32:25 -0300

    Create A.cpp

---
+++ Codeforces/Div4/Contest2009/A.cpp
@@ -0,0 +1,30 @@
+// https://codeforces.com/contest/2009/problem/A
+// A - Minimize!
+
+#include <bits/stdc++.h>
+
+#define int int64_t
+const int INF = 1ll<<55;
+
+void solve(){
+    int a,b;
+
+    std::cin>>a>>b;
+
+    std::cout<<b-a<<'\n';
+
+    return;
+}
+
+int32_t main(){
+
+    std::ios::sync_with_stdio(0), std::cin.tie(0);
+
+    int t;
+    std::cin>>t;
+
+    while(t--)
+        solve();
+
+    return 0;
+}
